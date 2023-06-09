aeadc6a254980ff5ad46743335bfcaf6eb8139d6
Vijay J <81014244+yoyozaemon@users.noreply.github.com>
2023-06-09 12:01:54 +0530

    About Section is added

---
+++ src/components/About.js
@@ -0,0 +1,47 @@
+import React, { Fragment } from 'react'
+
+function About() {
+  return (
+    <Fragment>
+        <div className="col py-3 d-flex align-items-center about-image"> 
+
+            <div className="container">
+                <div className="row">
+
+                    <div className="col-lg-6 col-md-12 col-sm-12 d-flex align-items-center">
+                        <div>
+                            <h1 className="font-big text-red slide-in-top">About me</h1>
+                            <hr></hr>
+                            <p className='slide-in-bottom'>Currently, Pursing my BTech Degree in PES University<br></br>
+                                with one year of experience in web developement.
+                            </p>
+                            <p className='slide-in-bottom'>
+                                Currently looking for new opportunities in industry and willing<br></br>
+                                to work on challenging and diverse projects with the latest technologies.
+                            </p>
+                            <p className='slide-in-bottom'>
+                                Previously worked as Game developer.<br></br>
+                                The technology I've worked on : <span className="text-red ls-2">Unity Engine, <br></br>
+                                C++, C, Rust. </span>
+                            </p>
+                            <p className='slide-in-bottom'>
+                                Currently Learning React Js Library, Flutter and Dart
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 col-md-12 col-sm-12">
+                        <img className="img-fluid" src="../images/nomad-amico.png" alt="..."></img>
+                    </div>
+
+
+
+                </div>
+            </div>
+
+            
+        </div>
+    </Fragment>
+  )
+}
+
+export default About
